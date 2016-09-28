d9f54a94231a870fd19626f9be53e7d5c8ab03fe
Lindsey <thelindseyreid@gmail.com>
2016-09-27 18:15:01 -0700

    Added Unity project
    yup

---
+++ I Ruff You 2/Assets/Editor/DialogueTool.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class DialogueTool : EditorWindow
+{
+    string _titleString = "Dialogue Tool";
+    
+    [MenuItem("Window/Dialogue Tool")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(DialogueTool));
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label(_titleString, EditorStyles.boldLabel);
+    }
+
+}
---
+++ I Ruff You 2/Assets/Scripts/Dialogue/DialogueNode.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+public class DialogueNode : MonoBehaviour {
+
+    DialogueNode Next;
+
+}
